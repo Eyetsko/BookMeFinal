8c9d841734fd0e03a49665257b99a914b44acfc6
Emily Yetsko <emilyyetsko@gmail.com>
2023-05-29 18:53:03 +0200

    added partials - header, footer, book view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,20 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
+
   def new
+    @book = Book.new
   end
+
   def create
+    @book = Book.new(book_params)
+    @book = Book.save
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
   end
 end
