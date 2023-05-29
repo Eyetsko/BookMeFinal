23ed7f173bc0f9edf852a5b3f59a9110fcb05c2d
Emily Yetsko <emilyyetsko@gmail.com>
2023-05-30 00:02:45 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :user do
-  resources :book 
-  resources :rentals
+  resources :books do
+    resources :rentals
   end
 end
