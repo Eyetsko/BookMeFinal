7d6b53218871a8fdd5dba2f4f06cf2ed588dd743
Esther Mueni <emueni97@gmail.com>
2023-05-30 12:24:32 +0200

    Added routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :user do
-  resources :book 
-  resources :rentals
+    resources :book
+    resources :rentals
   end
 end
