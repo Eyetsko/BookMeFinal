7a7cfdacab845bb57d7587193bc4c74a9c3aca8a
anasa <aroquesa@gmail.com>
2023-05-30 13:07:05 +0200

    book controller and a bit of view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,11 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book = Book.save
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
