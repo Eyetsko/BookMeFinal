b0e4e7d2ce0aa72e234526fd3fe6e488c8631526
anasa <aroquesa@gmail.com>
2023-05-31 12:28:24 +0200

    created calendar with datepicker

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -14,7 +14,9 @@ class RentalsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @rental = Rental.new(rental_params)
+    start_date = params[:rental][:start_date].split(" to ")[0]
+    end_date = params[:rental][:start_date].split(" to ")[1]
+    @rental = Rental.new(start_date: start_date, end_date: end_date)
     @rental.book = @book
     @rental.user = current_user
     if @rental.save!
@@ -27,7 +29,7 @@ class RentalsController < ApplicationController
   private
 
   def rental_params
-    params.require(:rental).permit(:start_date, :end_date, :book_id, :user_id)
+    params.require(:rental).permit(:book_id)
   end
 
   # def set_book
