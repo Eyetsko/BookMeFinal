16bd558857c806bd5a8042efce94c975160bc636
anasa <aroquesa@gmail.com>
2023-05-31 15:27:14 +0200

    view page of the book with calendar

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,16 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
   private
 
   def book_params
