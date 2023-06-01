977eae7b6ab1b26ba5c8aaad9eafe70f99cef195
anasa <aroquesa@gmail.com>
2023-06-01 09:53:25 +0200

    delete method for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,6 +32,12 @@ class BooksController < ApplicationController
     redirect_to book_path(@book)
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
   private
 
   def book_params
